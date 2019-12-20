11b406129b5a8adfb40e4dac2e16b802d13c220d
JoeNik <154469502@qq.com>
2019-12-20 18:22:56 +0800

    zk8 后台

--- venv/webtest.py
+++ venv/webtest.py
@@ -3,6 +3,10 @@ from urllib import request
 import wx, json
 import requests
 import time
+from http import cookiejar  # 保存cookie用的
+from urllib import parse  # 转译
+import urllib.request  # 请求库
+import execjs,js2py
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
@@ -10,6 +14,78 @@ headers = {
 
 cookiestrHead = r'ki1e_2132_saltkey=lsBSSV39; ki1e_2132_lastvisit=1559725160; ki1e_2132_auth=c705jfbJcHk%2B3iB3qmrPG1BrW5ijmJ6TRrgoHFm3EvazA85CtaRR%2BPp9LAoFz0Gr11Bm9hCt8iXL0eErm8Kyb9OcApg; ki1e_2132_connect_is_bind=1; ki1e_2132_smile=1D1; ki1e_2132_pc_size_c=0; Hm_lvt_da6569f688ba2c32429af00afd9eb8a1=1559780847,1559810342,1559810796,1560127058; ki1e_2132_creditnotice=0D0D0D0D0D0D0D0D0D538097; ki1e_2132_creditbase=0D1303D0D0D0D0D0D0D0; ki1e_2132_clearUserdata=forum; ki1e_2132_connect_not_sync_t=1; ki1e_2132_creditrule=%E5%8F%91%E8%A1%A8%E5%9B%9E%E5%A4%8D; td_cookie=18446744071559493794; timestamp=1560130076000; sign=4CBD74A5DD4643A9A56601CC25825DB4; ki1e_2132_nofocus_forum=1; ki1e_2132_atarget=1; ki1e_2132_forum_lastvisit=D_26_1560131402; ki1e_2132_ulastactivity=1560132614%7C0; ki1e_2132_checkpm=1; ki1e_2132_sendmail=1; ki1e_2132_lastcheckfeed=538097%7C1560132615; ki1e_2132_checkfollow=1; ki1e_2132_noticeTitle=1; Hm_lpvt_da6569f688ba2c32429af00afd9eb8a1=1560132616; amvid=c3faf2181a26daf6a98851670ae53153; ki1e_2132_lastact=1560132619%09forum.php%09viewthread; ki1e_2132_viewid=tid_6113962'
 DingPost_url = "https://oapi.dingtalk.com/robot/send?access_token=b63c5a144e9102029af7ef052c20150503441101f54dbb1c81a47285d56105d9"
+
+
+# key 13位UNIX时间 data 密码
+def get_des_psswd(data, key):
+    try:
+        jsstr = get_js()
+        ctx = execjs.compile(jsstr)  # 加载JS文件
+        return (ctx.call('do_encrypt_rc4', data, key))  # 调用js方法  第一个参数是JS的方法名，后面的data和key是js方法的参数
+       #return execjs.compile(open(r"E:\\bReadyWorking\\gothonweb\\bin\\loginCheck.js").read().decode("utf-8")).call('do_encrypt_rc4', data,key)
+    except Exception as e:
+        print('GetDingMarkDown error:' + str(e))
+
+def js2pyTest(src,key):
+    try:
+        data = open('E:\\bReadyWorking\\gothonweb\\bin\\loginCheck.js', 'r', encoding='utf8').read()
+
+        print(type(data))
+
+        data = js2py.eval_js(data)
+
+        print(data(src, key))
+    except Exception as e:
+        print('js2pyTest error:' + str(e))
+
+
+
+def get_js():
+    try:
+        f = open("E:\\bReadyWorking\\gothonweb\\bin\\loginCheck.js", 'r', encoding='utf-8')  # 打开JS文件
+        line = f.readline()
+        htmlstr = ''
+        while line:
+            htmlstr = htmlstr + line
+            line = f.readline()
+        return htmlstr
+    except Exception as e:
+        print('GetDingMarkDown error:' + str(e))
+
+
+#current_milli_time = lambda: int(round(time.time() * 1000))
+millis = int(round(time.time() * 1000))
+psw = js2pyTest("123456", millis)
+
+# 1.代码登录
+# 1.1 登录的网址
+login_url = "http://191.168.4.1/ac_portal/disclaimer/pc.html?template=disclaimer&tabs=pwd&vlanid=0&_ID_=0&switch_url=&url=https://hao.360.com/?wd_xp1&controller_type=&mac=00-00-00-00-00-00"
+login_form_data = {
+    "opr": "pwdLogin",
+    "userName": "郑乔",
+    "pwd": "ee772406c049",
+    "auth_tag": "1576831030768",
+    "rememberPwd": 0
+}
+# auth_tag 取到毫秒转化成Unix时间
+
+
+# 1.3 发送登录请求POST
+cook_jar = cookiejar.CookieJar()
+# 定义有添加 cook 功能的 处理器
+cook_hanlder = urllib.request.HTTPCookieProcessor(cook_jar)
+# 根据处理器 生成 opener
+opener = urllib.request.build_opener(cook_hanlder)
+
+# 带着参数 发送post请求
+# 添加请求头
+headers2 = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3650.400 QQBrowser/10.4.3341.400"}
+# 1 参数 将来 需要转译 转码； 2 post 请求的 data 要求是bytes
+login_str = parse.urlencode(login_form_data).encode('utf-8')
+login_request = urllib.request.Request(login_url, headers=headers2, data=login_str)
+# 如果登录成功，cookjar自动保存cookie
+opener.open(login_request)
 
 
 def get_contents(chapter):
